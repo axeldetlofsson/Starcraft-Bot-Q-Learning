fbb40a8d498bd8e4563559ddaa472b35c05996d3
Axel Detlofsson <axeldetlof@gmail.com>
2019-03-07 20:50:40 +0100

    minor changes

--- qbot.py
+++ qbot.py
@@ -1,9 +1,11 @@
 import random
 import math
 
+from absl import app
 import numpy as np
 import pandas as pd
 
+from pysc2.env import sc2_env
 from pysc2.agents import base_agent
 from pysc2.lib import actions
 from pysc2.lib import features
@@ -23,7 +25,7 @@ _PLAYER_ID = features.SCREEN_FEATURES.player_id.index
 _PLAYER_SELF = 1
 
 _TERRAN_COMMANDCENTER = 18
-_TERRAN_SCV = 45 
+_TERRAN_SCV = 45
 _TERRAN_SUPPLY_DEPOT = 19
 _TERRAN_BARRACKS = 21
 
@@ -78,6 +80,7 @@ class QLearningTable:
     def learn(self, s, a, r, s_):
         self.check_state_exist(s_)
         self.check_state_exist(s)
+        q_predict = self.q_table.ix[s, a]
         q_target = r + self.gamma * self.q_table.ix[s_, :].max()
         # update
         self.q_table.ix[s, a] += self.lr * (q_target - q_predict)
@@ -101,9 +104,9 @@ class SmartAgent(base_agent.BaseAgent):
         return [x + x_distance, y + y_distance]
     def step(self, obs):
         super(SmartAgent, self).step(obs)
-        player_y, player_x = (obs.observation['minimap'][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
+        player_y, player_x = (obs.observation['feature_minimap'][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
         self.base_top_left = 1 if player_y.any() and player_y.mean() <= 31 else 0
-        unit_type = obs.observation['screen'][_UNIT_TYPE]
+        unit_type = obs.observation['feature_screen'][_UNIT_TYPE]
 
         depot_y, depot_x = (unit_type == _TERRAN_SUPPLY_DEPOT).nonzero()
         supply_depot_count = supply_depot_count = 1 if depot_y.any() else 0
@@ -120,81 +123,91 @@ class SmartAgent(base_agent.BaseAgent):
             supply_limit,
             army_supply,
         ]
-        
         if self.previous_action is not None:
             reward = 0
-                
             if killed_unit_score > self.previous_killed_unit_score:
                 reward += KILL_UNIT_REWARD
-                    
             if killed_building_score > self.previous_killed_building_score:
                 reward += KILL_BUILDING_REWARD
-                
             self.qlearn.learn(str(self.previous_state), self.previous_action, reward, str(current_state))
-        
         rl_action = self.qlearn.choose_action(str(current_state))
         smart_action = smart_actions[rl_action]
-        
         self.previous_killed_unit_score = killed_unit_score
         self.previous_killed_building_score = killed_building_score
         self.previous_state = current_state
         self.previous_action = rl_action
-        
         if smart_action == ACTION_DO_NOTHING:
             return actions.FunctionCall(_NO_OP, [])
 
         elif smart_action == ACTION_SELECT_SCV:
-            unit_type = obs.observation['screen'][_UNIT_TYPE]
+            unit_type = obs.observation['feature_screen'][_UNIT_TYPE]
             unit_y, unit_x = (unit_type == _TERRAN_SCV).nonzero()
-                
             if unit_y.any():
                 i = random.randint(0, len(unit_y) - 1)
                 target = [unit_x[i], unit_y[i]]
-                
                 return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
-        
         elif smart_action == ACTION_BUILD_SUPPLY_DEPOT:
             if _BUILD_SUPPLY_DEPOT in obs.observation['available_actions']:
-                unit_type = obs.observation['screen'][_UNIT_TYPE]
+                unit_type = obs.observation['feature_screen'][_UNIT_TYPE]
                 unit_y, unit_x = (unit_type == _TERRAN_COMMANDCENTER).nonzero()
-                
                 if unit_y.any():
                     target = self.transformLocation(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
-                
                     return actions.FunctionCall(_BUILD_SUPPLY_DEPOT, [_NOT_QUEUED, target])
-        
         elif smart_action == ACTION_BUILD_BARRACKS:
             if _BUILD_BARRACKS in obs.observation['available_actions']:
-                unit_type = obs.observation['screen'][_UNIT_TYPE]
+                unit_type = obs.observation['feature_screen'][_UNIT_TYPE]
                 unit_y, unit_x = (unit_type == _TERRAN_COMMANDCENTER).nonzero()
-                
                 if unit_y.any():
                     target = self.transformLocation(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
-            
                     return actions.FunctionCall(_BUILD_BARRACKS, [_NOT_QUEUED, target])
-    
         elif smart_action == ACTION_SELECT_BARRACKS:
-            unit_type = obs.observation['screen'][_UNIT_TYPE]
+            unit_type = obs.observation['feature_screen'][_UNIT_TYPE]
             unit_y, unit_x = (unit_type == _TERRAN_BARRACKS).nonzero()
-                
             if unit_y.any():
                 target = [int(unit_x.mean()), int(unit_y.mean())]
-        
                 return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
-        
         elif smart_action == ACTION_BUILD_MARINE:
             if _TRAIN_MARINE in obs.observation['available_actions']:
                 return actions.FunctionCall(_TRAIN_MARINE, [_QUEUED])
-        
         elif smart_action == ACTION_SELECT_ARMY:
             if _SELECT_ARMY in obs.observation['available_actions']:
                 return actions.FunctionCall(_SELECT_ARMY, [_NOT_QUEUED])
-        
         elif smart_action == ACTION_ATTACK:
             if _ATTACK_MINIMAP in obs.observation["available_actions"]:
                 if self.base_top_left:
                     return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, [39, 45]])
-            
                 return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, [21, 24]])
-        
         return actions.FunctionCall(_NO_OP, [])
+
+def main(unused_argv):
+  agent = SmartAgent()
+  try:
+    while True:
+      with sc2_env.SC2Env(
+          map_name="Simple64",
+          players=[sc2_env.Agent(sc2_env.Race.terran),
+                   sc2_env.Bot(sc2_env.Race.random,
+                               sc2_env.Difficulty.very_easy)],
+          agent_interface_format=features.AgentInterfaceFormat(
+              feature_dimensions=features.Dimensions(screen=84, minimap=64),
+              use_feature_units=True),
+          step_mul=16,
+          game_steps_per_episode=0,
+          visualize=True) as env:
+
+        agent.setup(env.observation_spec(), env.action_spec())
+
+        timesteps = env.reset()
+        agent.reset()
+
+        while True:
+          step_actions = [agent.step(timesteps[0])]
+          if timesteps[0].last():
+            break
+          timesteps = env.step(step_actions)
+
+  except KeyboardInterrupt:
+    pass
+
+if __name__ == "__main__":
+  app.run(main)
